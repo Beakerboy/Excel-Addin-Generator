b578dcd77d846668611f5224cf75b23181f0aea5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-23 16:46:45 -0400

    Update excelAddinGenerator.py

--- src/excelAddinGenerator.py
+++ src/excelAddinGenerator.py
@@ -1,20 +1,25 @@
 import shutil
 import sys
+
 # check the extension on sys.argv[1] to determine which function to call
-# if .xlam
-#     createFromZip(sys.argv[1], sys.argv[2])
-# else if .bin
-#     createFromBin(sys.argv[1], sys.argv[2])
-
-def createFromBin(input, output)
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+if input_file.endswith('.xlam'):
+    createFromZip(input_file, output_file)
+elif input_file.endswith('.bin'):
+    createFromBin(input_file, output_file)
+else:
+    print input_file, " is not a valid file format."
+        
+def createFromBin(input_file, output_file)
     # check that input is a binary file or the correct type
-    shutil.move(input, "data/xl/vbaProject.bin")
-    shutil.makearchive(output, 'zip', "data")
+    shutil.move(input_file, "data/xl/vbaProject.bin")
+    shutil.makearchive(output_file, 'zip', "data")
 
-def createFromZip(input, output)
+def createFromZip(input_file, output_file)
     # check that input is a zip file
     # check that the zip archive contains /xl/vbaProject.bin
     # binFile = {extract vbaProject.bin from input}
     shutil.move(binFile, "data/xl/vbaProject.bin")
-    shutil.makearchive(output, 'zip', "data")
+    shutil.makearchive(output_file, 'zip', "data")
 
