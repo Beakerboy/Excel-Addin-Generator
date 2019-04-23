5dd00fafb8f8fc0230b017fd0f39ac416b5fb455
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-23 11:11:48 -0400

    Update excelAddinGenerator.py

--- src/excelAddinGenerator.py
+++ src/excelAddinGenerator.py
@@ -1,4 +1,5 @@
 import shutil
 import sys
+# Check that sys.argv[1] is a binary file or a zip archive with a binary file in the correct directory
 shutil.move(sys.argv[1], "data/xl/vbaProject.bin")
 shutil.makearchive(sys.argv[2], 'zip', "data")
