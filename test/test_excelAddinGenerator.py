10747d47a6236927e88b06af3dac30cfbcc33d17
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-23 11:24:30 -0400

    Create test_excelAddinGenerator.py

---
+++ test/test_excelAddinGenerator.py
@@ -0,0 +1,16 @@
+# test_excelAddinGenerator.py
+
+import pytest
+from excelAddinGenerator import *
+
+def test_success_from_bin():
+    # python excelAddinGenerator.py ./vbaProject.bin ./success_bin.xlam
+    
+def test_success_from_xlam():
+    # python excelAddinGenerator.py ./test.xlam ./succes_xlas.xlam
+
+def test_bin_not_bin():
+    # python excelAddinGenerator.py ./notbin.bin ./fail.xlam
+
+def test_xlam_not_zip():
+    # python excelAddinGenerator.py ./notzip.xlam ./fail.xlam
