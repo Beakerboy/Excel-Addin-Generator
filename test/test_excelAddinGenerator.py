6eb1758c611513dc3eb6f6ab52ad95467dfac300
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-23 16:53:11 -0400

    Update test_excelAddinGenerator.py

--- test/test_excelAddinGenerator.py
+++ test/test_excelAddinGenerator.py
@@ -1,7 +1,7 @@
 # test_excelAddinGenerator.py
 
 import pytest
-from excelAddinGenerator import *
+from ../src/excelAddinGenerator import *
 
 def test_success_from_bin():
     # python excelAddinGenerator.py ./vbaProject.bin ./success_bin.xlam
