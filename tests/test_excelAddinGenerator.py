d968ad962d3ae0a09cf2f426dcb7d0f69ec0b32f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-24 10:53:07 -0400

    whitespace

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -10,7 +10,7 @@ def test_success_from_bin():
 def test_fail_from_bin():
     with pytest.raises(Exception) as e_info:
     # python excelAddinGenerator.py ./test.xlam ./succes_xlam.xlam
-    createFromBin("tests/test.xlam", "success_xlam.xlam")
+        createFromBin("tests/test.xlam", "success_xlam.xlam")
 
 #def test_bin_not_bin():
     # python excelAddinGenerator.py ./notbin.bin ./fail.xlam
