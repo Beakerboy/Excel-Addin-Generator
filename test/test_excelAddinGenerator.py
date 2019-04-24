5984e51d32fdb4a15507f89604e735906225a643
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-24 06:59:36 -0400

    Update test_excelAddinGenerator.py

--- test/test_excelAddinGenerator.py
+++ test/test_excelAddinGenerator.py
@@ -5,7 +5,7 @@ from excelAddinGenerator.main import *
 
 def test_success_from_bin():
     # python excelAddinGenerator.py ./vbaProject.bin ./success_bin.xlam
-    createFromBin("./vbaProject.bin", "success_bin.xlam")
+    createFromBin("test/vbaProject.bin", "success_bin.xlam")
     
 def test_success_from_xlam():
     # python excelAddinGenerator.py ./test.xlam ./succes_xlam.xlam
