07e69aaf51f387fd19dffd46a56dc0222651bbb9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-24 15:16:08 -0400

    Update test_excelAddinGenerator.py

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -5,13 +5,13 @@ from excelAddinGenerator.main import *
 
 def test_success_from_bin():
     """Test that xlam is successfully generated from a OLE file"""
-    createFromBin("tests/vbaProject.bin", "success_bin.xlam")
+    createFromBin("tests/vbaProject.bin", "src/data", "success_bin.xlam")
     # Assert that xlam file is created
     
 def test_fail_from_bin():
     """ Test that an exception is thrown if the file is not an OLE file"""
     with pytest.raises(Exception) as e_info:
-        createFromBin("tests/test.xlam", "success_xlam.xlam")
+        createFromBin("tests/test.xlam", "src/data", "success_xlam.xlam")
 
 #def test_bin_not_bin():
     # python excelAddinGenerator.py ./notbin.bin ./fail.xlam
