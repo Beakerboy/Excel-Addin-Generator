12a9f6842746ce41af897954ad4025182da8c072
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-06 15:39:03 -0500

    Add tests (#5)

--- excelAddinGenerator/main.py
+++ excelAddinGenerator/main.py
@@ -4,33 +4,35 @@ def main():
     if len(sys.argv) > 2:
         # check the extension on sys.argv[1] to determine which function to call
         input_file = sys.argv[1]
-        output_file = sys.argv[2]
+        output_file_name = sys.argv[2]
         if input_file.endswith('.xlam'):
-            createFromZip(input_file, output_file)
+            createFromZip(input_file, output_file_name)
         elif input_file.endswith('.bin'):
-            createFromBin(input_file, os.path.dirname(sys.argv[0]) + '/../src/data', output_file)
+            createFromBin(input_file, os.path.dirname(sys.argv[0]) + '/../src/data', output_file_name)
         else:
             raise Exception(input_file, " is not a valid file format.")
 
-def createFromBin(input_file, wrapper_dir, output_file):
+def createFromBin(input_file, wrapper_dir, output_file_name):
     """Create a zip file containing the provided bin"""
-    # check that input is an OLE file.
+    # check that inputfile has at least 8 bytes
+    if os.path.getsize(input_file) < 8:
+        raise Exception('Bin file too small')
     # file must start with 'd0 cf 11 e0 a1 b1 1a e1'
     fileSig = open(input_file, "rb").read(8).hex()
     if fileSig != 'd0cf11e0a1b11ae1':
         raise Exception('File signature {} is not as expected.', format(fileSig))
     shutil.move(input_file, wrapper_dir + "/xl/vbaProject.bin")
-    shutil.make_archive(output_file, 'zip', wrapper_dir)
-    shutil.move(output_file + ".zip", output_file)
+    shutil.make_archive(output_file_name, 'zip', wrapper_dir)
+    shutil.move(output_file_name + ".zip", output_file_name)
 
-def createFromZip(input_file, output_file):
+def createFromZip(input_file, wrapper_dir, output_file_name):
     """Create a zip file containing the bin file within the provided zip file"""
     # check that input is a zip file
     if zipfile.is_zipfile(input_file):
         # check that the zip archive contains /xl/vbaProject.bin
         with zipfile.ZipFile(input_file, 'r') as zip:
-          binFile = zip.read('xl/vbaProject.bin')
-          createFromBin(binFile, os.path.dirname(sys.argv[0]) + '/../src/data', output_file)
+          zip.extract('xl/vbaProject.bin')
+          createFromBin('xl/vbaProject.bin', wrapper_dir, output_file_name)
     else:
         raise Exception(input_file, " is not a valid file format.")
 
--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -2,19 +2,27 @@
 
 import pytest
 from excelAddinGenerator.main import *
+from os.path import exists
 
 def test_success_from_bin():
     """Test that xlam is successfully generated from a OLE file"""
     createFromBin("tests/vbaProject.bin", "src/data", "success_bin.xlam")
     # Assert that xlam file is created
+    assert exists("success_bin.xlam")
+    createFromZip("success_bin.xlam", "src/data", "success_xlam.xlam")
+    assert exists("success_xlam.xlam")
     
-def test_fail_from_bin():
-    """ Test that an exception is thrown if the file is not an OLE file"""
+def test_not_bin_exception():
+    """ Test that an exception is thrown if the bin file is not an OLE file"""
     with pytest.raises(Exception) as e_info:
-        createFromBin("tests/test.xlam", "src/data", "success_xlam.xlam")
+        createFromBin("tests/blank.bin", "src/data", "./fail.xlam")
 
-#def test_bin_not_bin():
-    # python excelAddinGenerator.py ./notbin.bin ./fail.xlam
-
-#def test_xlam_not_zip():
-    # python excelAddinGenerator.py ./notzip.xlam ./fail.xlam
+def test_not_bin_exception():
+    """ Test that an exception is thrown if the bin file is not an OLE file"""
+    with pytest.raises(Exception) as e_info:
+        createFromBin("tests/not_bin.bin", "src/data", "./fail.xlam")
+        
+def test_xlam_not_zip():
+    """ Test that an exception is thrown if the zip is not a zip archive"""
+    with pytest.raises(Exception) as e_info:
+        createFromZip("tests/blank.bin", "src/data", "./fail.xlam")
