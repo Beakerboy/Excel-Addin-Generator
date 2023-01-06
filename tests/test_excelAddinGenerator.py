b1b1e0c8a407dde3945cfb7984817ac80fab035f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-06 15:01:47 -0500

    Update test_excelAddinGenerator.py

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -9,19 +9,20 @@ def test_success_from_bin():
     createFromBin("tests/vbaProject.bin", "src/data", "success_bin.xlam")
     # Assert that xlam file is created
     assert exists("success_bin.xlam")
+    createFromZip("tests/success_bin.xlam", "success_xlam.xlam")
+    assert exists("success_xlam.xlam")
     
 def test_not_bin_exception():
     """ Test that an exception is thrown if the bin file is not an OLE file"""
     with pytest.raises(Exception) as e_info:
         createFromBin("tests/blank.bin", "src/data", "./fail.xlam")
 
+def test_not_bin_exception():
+    """ Test that an exception is thrown if the bin file is not an OLE file"""
+    with pytest.raises(Exception) as e_info:
+        createFromBin("tests/not_bin.bin", "src/data", "./fail.xlam")
+        
 def test_xlam_not_zip():
     """ Test that an exception is thrown if the zip is not a zip archive"""
     with pytest.raises(Exception) as e_info:
         createFromZip("tests/blank.bin", "./fail.xlam")
-        
-def test_success_from_xlam():
-    """Test that xlam is successfully generated from a xlam file"""
-    createFromZip("tests/test.xlam", "success_xlam.xlam")
-    # Assert that xlam file is created
-    assert exists("success_xlam.xlam")
