56e11c5ade7e901295b717d99237aa08f8c837f2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-06 15:11:34 -0500

    Update main.py

--- excelAddinGenerator/main.py
+++ excelAddinGenerator/main.py
@@ -4,15 +4,15 @@ def main():
     if len(sys.argv) > 2:
         # check the extension on sys.argv[1] to determine which function to call
         input_file = sys.argv[1]
-        output_file = sys.argv[2]
+        output_file_name = sys.argv[2]
         if input_file.endswith('.xlam'):
-            createFromZip(input_file, output_file)
+            createFromZip(input_file, output_file_name)
         elif input_file.endswith('.bin'):
-            createFromBin(input_file, os.path.dirname(sys.argv[0]) + '/../src/data', output_file)
+            createFromBin(input_file, os.path.dirname(sys.argv[0]) + '/../src/data', output_file_name)
         else:
             raise Exception(input_file, " is not a valid file format.")
 
-def createFromBin(input_file, wrapper_dir, output_file):
+def createFromBin(input_file, wrapper_dir, output_file_name):
     """Create a zip file containing the provided bin"""
     # check that inputfile has at least 8 bytes
     if os.path.getsize(input_file) < 8:
@@ -23,16 +23,16 @@ def createFromBin(input_file, wrapper_dir, output_file):
         raise Exception('File signature {} is not as expected.', format(fileSig))
     shutil.move(input_file, wrapper_dir + "/xl/vbaProject.bin")
     shutil.make_archive(output_file, 'zip', wrapper_dir)
-    shutil.move(output_file + ".zip", output_file)
+    shutil.move(output_file + ".zip", output_file_name)
 
-def createFromZip(input_file, output_file):
+def createFromZip(input_file, output_file_name):
     """Create a zip file containing the bin file within the provided zip file"""
     # check that input is a zip file
     if zipfile.is_zipfile(input_file):
         # check that the zip archive contains /xl/vbaProject.bin
         with zipfile.ZipFile(input_file, 'r') as zip:
           binFile = zip.read('xl/vbaProject.bin')
-          createFromBin(binFile, os.path.dirname(sys.argv[0]) + '/../src/data', output_file)
+          createFromBin(binFile, os.path.dirname(sys.argv[0]) + '/../src/data', output_file_name)
     else:
         raise Exception(input_file, " is not a valid file format.")
 
