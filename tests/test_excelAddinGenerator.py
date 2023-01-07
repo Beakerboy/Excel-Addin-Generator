bca3e139a377b0050ff73f064e9158afe519b226
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-01-06 19:04:38 -0500

    File hash (#7)

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -3,17 +3,23 @@
 import pytest
 from excelAddinGenerator.main import *
 from os.path import exists
+from filehash import FileHash
 
 def test_success_from_bin():
     """Test that xlam is successfully generated from a OLE file"""
     createFromBin("tests/vbaProject.bin", "src/data", "success_bin.xlam")
     # Assert that xlam file is created
     assert exists("success_bin.xlam")
-    #ToDo: assert that bin file within success_bin.xlam matches tests/vbaProject.bin
-    
+    #assert that bin file within success_bin.xlam matches tests/vbaProject.bin
+    extractBinFromZip("success_bin.xlam")
+    md5hasher = FileHash('md5')
+    assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
+
     createFromZip("success_bin.xlam", "src/data", "success_xlam.xlam")
     assert exists("success_xlam.xlam")
     #assert that bin file within success_xlam.xlam matches bin file within success_bin.xlam
+    extractBinFromZip("success_xlam.xlam")
+    assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
     
 def test_not_bin_exception():
     """ Test that an exception is thrown if the bin file is not an OLE file"""
