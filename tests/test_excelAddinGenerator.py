010d6225ab5707e8a59a3bb6879902728d115395
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-03 08:17:40 -0400

    Update test_excelAddinGenerator.py

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -10,31 +10,31 @@ def test_success_from_bin():
     # Assert that xlam file is created
     assert exists("success_bin.xlam")
     #assert that bin file within success_bin.xlam matches tests/vbaProject.bin
-    excel_addin_generator.extractBinFromZip("success_bin.xlam")
+    excel_addin_generator.main.extractBinFromZip("success_bin.xlam")
     md5hasher = FileHash('md5')
     assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
 
     excel_addin_generator.createFromZip("success_bin.xlam", "src/data", "success_xlam.xlam")
     assert exists("success_xlam.xlam")
     #assert that bin file within success_xlam.xlam matches bin file within success_bin.xlam
-    excel_addin_generator.extractBinFromZip("success_xlam.xlam")
+    excel_addin_generator.main.extractBinFromZip("success_xlam.xlam")
     assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
     
 def test_not_bin_exception():
     """ Test that an exception is thrown if the bin file is not an OLE file"""
     with pytest.raises(Exception) as e_info:
-        excel_addin_generator.createFromBin("tests/blank.bin", "src/data", "./fail.xlam")
+        excel_addin_generator.main.createFromBin("tests/blank.bin", "src/data", "./fail.xlam")
         
 def test_xlam_not_zip():
     """ Test that an exception is thrown if the zip is not a zip archive"""
     with pytest.raises(Exception) as e_info:
-        excel_addin_generator.createFromZip("tests/blank.bin", "src/data", "./fail.xlam")
+        excel_addin_generator.main.createFromZip("tests/blank.bin", "src/data", "./fail.xlam")
 
 def test_main():
-    excel_addin_generator.main(["./excelAddinGenerator", "./tests/vbaProject.bin", "success_bin.xlam"])
-    excel_addin_generator.main(["./excelAddinGenerator", "success_bin.xlam", "success_xlam.xlam"])
+    excel_addin_generator.main.main(["./excelAddinGenerator", "./tests/vbaProject.bin", "success_bin.xlam"])
+    excel_addin_generator.main.main(["./excelAddinGenerator", "success_bin.xlam", "success_xlam.xlam"])
 
 def test_main_incorrect_type():
     """ Test that an exception is thrown if the zip is not a zip archive"""
     with pytest.raises(Exception) as e_info:
-        excel_addin_generator.main(["./excelAddinGenerator", "./src/data/xl/styles.xml", "fail.xlam"])
+        excel_addin_generator.main.main(["./excelAddinGenerator", "./src/data/xl/styles.xml", "fail.xlam"])
