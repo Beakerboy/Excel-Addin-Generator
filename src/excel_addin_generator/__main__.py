0655d8f471c2ea38d16d17a7a3ba02a9b0df257a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-03 10:10:33 -0400

    Create __main__.py

---
+++ src/excel_addin_generator/__main__.py
@@ -0,0 +1,17 @@
+import excel_addin_generator.main as gen
+
+
+def main(args: list) -> None:
+
+    if len(args) > 2:
+        # check the extension on sys.argv[1] to determine which function to call
+        input_file = args[1]
+        output_file_name = args[2]
+        if input_file.endswith('.xlam'):
+            gen.createFromZip(input_file, args[0] + '/data', output_file_name)
+        elif input_file.endswith('.bin'):
+            gen.create_from_bin(input_file, args[0] + '/data', output_file_name)
+        else:
+            raise Exception(input_file, " is not a valid file format.")
+
+main()
