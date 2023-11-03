84f8df9fddd3c2bbd53ee8342ca5b67cedde07ba
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-03 10:11:00 -0400

    Update main.py

--- src/excel_addin_generator/main.py
+++ src/excel_addin_generator/main.py
@@ -4,21 +4,7 @@ import sys
 import zipfile
 
 
-def main(args: list) -> None:
-
-    if len(args) > 2:
-        # check the extension on sys.argv[1] to determine which function to call
-        input_file = args[1]
-        output_file_name = args[2]
-        if input_file.endswith('.xlam'):
-            createFromZip(input_file, args[0] + '/data', output_file_name)
-        elif input_file.endswith('.bin'):
-            create_from_bin(input_file, args[0] + '/data', output_file_name)
-        else:
-            raise Exception(input_file, " is not a valid file format.")
-
-
-def create_from_bin(input_file: str, wrapper_dir: str, output_file_name: str) -> None:
+ create_from_bin(input_file: str, wrapper_dir: str, output_file_name: str) -> None:
 
     """Create a zip file containing the provided bin"""
     # file must start with 'd0 cf 11 e0 a1 b1 1a e1'
@@ -46,11 +32,3 @@ def extractBinFromZip(input_file: str) -> None:
             zip.extract('xl/vbaProject.bin')
     else:
         raise Exception(input_file, " is not a valid file format.")
-
-
-if __name__ == "__main__":
-    args = []
-    args[0] = os.path.dirname(sys.argv[0])
-    args[1] = sys.argv[1]
-    args[2] = sys.argv[2]
-    main(args)
