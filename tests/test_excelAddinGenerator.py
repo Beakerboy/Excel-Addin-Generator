fd87e1175b3b01e865b1d3b9b260abc02188bfb2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-03 10:05:27 -0400

    Update test_excelAddinGenerator.py

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -15,7 +15,7 @@ def test_success_from_bin() -> None:
     md5hasher = FileHash('md5')
     assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
 
-    excel_addin_generator.main.createFromZip("success_bin.xlam", "src/data", "success_xlam.xlam")
+    excel_addin_generator.main.createFromZip("success_bin.xlam", "src/excel_addin_generator/data", "success_xlam.xlam")
     assert exists("success_xlam.xlam")
     # assert that bin file within success_xlam.xlam matches bin file within success_bin.xlam
     excel_addin_generator.main.extractBinFromZip("success_xlam.xlam")
@@ -25,13 +25,13 @@ def test_success_from_bin() -> None:
 def test_not_bin_exception() -> None:
     """ Test that an exception is thrown if the bin file is not an OLE file"""
     with pytest.raises(Exception):
-        excel_addin_generator.main.createFromBin("tests/blank.bin", "src/data", "./fail.xlam")
+        excel_addin_generator.main.createFromBin("tests/blank.bin", "src/excel_addin_generator/data", "./fail.xlam")
 
 
 def test_xlam_not_zip() -> None:
     """ Test that an exception is thrown if the zip is not a zip archive"""
     with pytest.raises(Exception):
-        excel_addin_generator.main.createFromZip("tests/blank.bin", "src/data", "./fail.xlam")
+        excel_addin_generator.main.createFromZip("tests/blank.bin", "src/excel_addin_generator/data", "./fail.xlam")
 
 
 def test_main() -> None:
@@ -42,4 +42,4 @@ def test_main() -> None:
 def test_main_incorrect_type() -> None:
     """ Test that an exception is thrown if the zip is not a zip archive"""
     with pytest.raises(Exception):
-        excel_addin_generator.main.main(["./src/excel_addin_generator", "./src/data/xl/styles.xml", "fail.xlam"])
+        excel_addin_generator.main.main(["./src/excel_addin_generator", "./src/excel_addin_generator/data/xl/styles.xml", "fail.xlam"])
