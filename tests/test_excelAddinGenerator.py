47b9f68563ac78ecd590f0490cf23f7d01dc057c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-03 08:56:03 -0400

    Update test_excelAddinGenerator.py

--- tests/test_excelAddinGenerator.py
+++ tests/test_excelAddinGenerator.py
@@ -10,14 +10,14 @@ def test_success_from_bin() -> None:
     excel_addin_generator.main.create_from_bin("tests/vbaProject.bin", "src/data", "success_bin.xlam")
     # Assert that xlam file is created
     assert exists("success_bin.xlam")
-    #assert that bin file within success_bin.xlam matches tests/vbaProject.bin
+    # assert that bin file within success_bin.xlam matches tests/vbaProject.bin
     excel_addin_generator.main.extractBinFromZip("success_bin.xlam")
     md5hasher = FileHash('md5')
     assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
 
     excel_addin_generator.main.createFromZip("success_bin.xlam", "src/data", "success_xlam.xlam")
     assert exists("success_xlam.xlam")
-    #assert that bin file within success_xlam.xlam matches bin file within success_bin.xlam
+    # assert that bin file within success_xlam.xlam matches bin file within success_bin.xlam
     excel_addin_generator.main.extractBinFromZip("success_xlam.xlam")
     assert md5hasher.hash_file("tests/vbaProject.bin") == md5hasher.hash_file("xl/vbaProject.bin")
 
